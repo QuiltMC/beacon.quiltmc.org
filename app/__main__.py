1956583eb4bc778a621a94fe49c7724a2b26b8b1
Ambre Bertucci <ambre@akarys.me>
2023-06-20 18:11:35 +0200

    Main app logic

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,38 @@
-def main() -> None:
-    """The main entry point."""
-    print("Hello, world!")
+import asyncio
+import os
+
+import asyncpg
+from aiohttp import web
+
+
+async def signal_handler(request: web.Request) -> web.Response:
+    """Handle the signal."""
+    pool = request.app["pool"]
+
+    async with pool.acquire() as conn:
+        await conn.execute("INSERT INTO signals (signal) VALUES (timezone('utc', now()))")
+
+    return web.Response(text="OK", status=200)
+
+
+async def main(loop: asyncio.AbstractEventLoop) -> web.Application:
+    """Construct the main applicaiton."""
+    dsn = os.environ.get("DATABASE_URL", None)
+
+    if not dsn:
+        raise RuntimeError("DSN not set")
+
+    app = web.Application()
+    app["pool"] = await asyncpg.create_pool(dsn, loop=loop)
+
+    async with app["pool"].acquire() as conn:
+        await conn.execute("CREATE TABLE IF NOT EXISTS signals (signal timestamptz)")
+
+    app.add_routes([web.post("/signal", signal_handler)])
+    return app
 
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    app = loop.run_until_complete(main(loop))
+    web.run_app(app, port=8000, loop=loop)
